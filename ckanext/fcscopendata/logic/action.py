a2d4d5c14ea77829c133ec75186f8d1e147397a5
sagarggh <sagar.ghimire@datopian.com>
2022-02-08 09:08:53 +0545

    [tags][xs] English keywords to the tags too when the form is submitted

---
+++ ckanext/fcscopendata/logic/action.py
@@ -0,0 +1,45 @@
+import logging
+import ckan.plugins.toolkit as tk
+import ckan.plugins as p
+import ckan.logic as logic
+
+_get_or_bust = logic.get_or_bust
+
+log = logging.getLogger(__name__)
+
+@p.toolkit.chained_action
+def package_create(up_func, context, data_dict):
+    # Get the translated title field value in the original title field so that 
+    # core features do not break. eg. solr search with title
+    data_dict['title'] =  data_dict.get('title_translated-en', '')
+    keywords =  [tag.strip() \
+                for tag in data_dict.get('keywords-en', '').split(',') \
+                if tag.strip()]
+    tags = [{ 'name': word, 'state': 'active'} for word in keywords ]
+    if tags:
+        data_dict['tags'] = tags 
+    result = up_func(context, data_dict)
+    return result
+
+@p.toolkit.chained_action
+def package_update(up_func, context, data_dict):
+    # Get the translated title field value in the original title field so that 
+    # core features do not break. eg. solr search with title 
+    data_dict['title'] =  data_dict.get('title_translated-en', '')
+    keywords =  [tag.strip() \
+                for tag in data_dict.get('keywords-en', '').split(',') \
+                if tag.strip()]
+    tags = [{ 'name': word, 'state': 'active'} for word in keywords ]
+    if tags:
+        data_dict['tags'] = tags 
+    result = up_func(context, data_dict)
+    return result
+
+
+def get_actions():
+    return {
+        'package_create': package_create,
+        'package_update': package_update,
+    }
+
+
